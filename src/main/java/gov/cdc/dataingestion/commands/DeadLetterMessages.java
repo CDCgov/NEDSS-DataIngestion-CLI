5c077550c33962530194e93b1caf1bbf77918411
Selvarasu Sathiah <140747625+ssathiah@users.noreply.github.com>
2023-11-23 03:59:59 -0500

    Updated for dlt messages

--- src/main/java/gov/cdc/dataingestion/commands/DeadLetterMessages.java
+++ src/main/java/gov/cdc/dataingestion/commands/DeadLetterMessages.java
@@ -10,9 +10,9 @@ import picocli.CommandLine;
 import java.util.List;
 import java.util.Properties;
 
-@CommandLine.Command(name = "dltmsgs", mixinStandardHelpOptions = true, description = "This functionality to view the messages in the dead letter messages.")
+@CommandLine.Command(name = "dltmessages", mixinStandardHelpOptions = true, description = "This functionality to view the messages in the dead letter messages.")
 public class DeadLetterMessages implements Runnable {
-    @CommandLine.Option(names = {"--msgsize"}, description = "Number of Messages to display", interactive = true, echo = true, required = false)
+    @CommandLine.Option(names = {"--msgsize"}, description = "Number of Messages to display.Default is 10", interactive = true, echo = true, required = false)
     String msgsize = "10";
     @CommandLine.Option(names = {"--username"}, description = "Admin Username to connect to DI service", interactive = true, echo = true, required = true)
     String username;
@@ -32,46 +32,48 @@ public class DeadLetterMessages implements Runnable {
 
                 authModel.setUsername(username.trim());
                 authModel.setPassword(password);
-                authModel.setServiceEndpoint(properties.getProperty("service.int1.dltErrorMessages"));
+                authModel.setServiceEndpoint(properties.getProperty("service.dltErrorMessages"));
 
-                String apiResponse = authUtil.getResponseFromDIService(authModel, "dltmsgs");
-                displayDLTMessages(apiResponse,msgsize);
-                
+                String apiResponse = authUtil.getResponseFromDIService(authModel, "dltmessages");
+                displayDLTMessages(apiResponse, msgsize);
+
             } else {
                 System.err.println("Username or password is empty.");
             }
         } else {
-            System.err.println("Username or password or HL7 file path is null.");
+            System.err.println("Username or password is null.");
         }
     }
-    private void displayDLTMessages(String dltMsgs,String msgSize){
-        int nonOfMsgDisplay=0;
-        if(msgSize!=null && !msgSize.isEmpty()){
-            nonOfMsgDisplay=Integer.parseInt(msgSize);
+
+    private void displayDLTMessages(String dltMsgs, String msgSize) {
+        if (dltMsgs != null && !dltMsgs.trim().startsWith("[")) {
+            System.out.println(dltMsgs);
+        } else {
+            int nonOfMsgDisplay = 0;
+            if (msgSize != null && !msgSize.isEmpty()) {
+                nonOfMsgDisplay = Integer.parseInt(msgSize);
+            }
+            JSONArray jsonArray = new JSONArray(dltMsgs);
+            int availableMsgSize = jsonArray.length();
+            if (nonOfMsgDisplay > availableMsgSize) {
+                nonOfMsgDisplay = availableMsgSize;
+            }
+            List errorSubList = jsonArray.toList().subList(0, nonOfMsgDisplay);
+            JSONArray subListJsonArray = new JSONArray(errorSubList);
+
+            JSONArray newJsonArray = new JSONArray();
+            for (int i = 0; i < subListJsonArray.length(); i++) {
+                JSONObject origObject = subListJsonArray.getJSONObject(i);
+
+                JSONObject newJsonObject = new JSONObject();
+                newJsonObject.put("ERROR_MSG_ID", origObject.get("errorMessageId"));
+                newJsonObject.put("ERROR_MSG_SOURCE", origObject.get("errorMessageSource"));
+                newJsonObject.put("ERROR_STACK_TRACE", origObject.get("errorStackTraceShort"));
+                newJsonObject.put("CREATED_ON", origObject.get("createdOn"));
+
+                newJsonArray.put(newJsonObject);
+            }
+            System.out.println(newJsonArray.toString());
         }
-        JSONArray jsonArray = new JSONArray(dltMsgs);
-        int availableMsgSize= jsonArray.length();
-        System.out.println("Total DLT Messages:"+availableMsgSize);
-        if(nonOfMsgDisplay>availableMsgSize){
-            nonOfMsgDisplay=availableMsgSize;
-        }
-        List errorSubList=jsonArray.toList().subList(0,nonOfMsgDisplay);
-        JSONArray subListJsonArray = new JSONArray(errorSubList);
-
-        JSONArray newJsonArray = new JSONArray();
-        for (int i = 0; i < subListJsonArray.length(); i++) {
-            JSONObject origObject = subListJsonArray.getJSONObject(i);
-
-            System.out.println(origObject.get("errorMessageId"));
-
-            JSONObject newJsonObject=new JSONObject();
-            newJsonObject.put("ERROR_MSG_ID",origObject.get("errorMessageId"));
-            newJsonObject.put("ERROR_MSG_SOURCE",origObject.get("errorMessageSource"));
-            newJsonObject.put("ERROR_STACK_TRACE",origObject.get("errorStackTraceShort"));
-            newJsonObject.put("CREATED_ON",origObject.get("createdOn"));
-
-            newJsonArray.put(newJsonObject);
-        }
-        System.out.println(newJsonArray.toString());
     }
 }
