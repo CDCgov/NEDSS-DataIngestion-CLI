b166361a1fd65f53f6d093b80e1737ba41f7d739
Mary McGrath <m.c.mcgrath13@gmail.com>
2026-02-19 13:43:18 -0500

    style: run formatting

--- src/main/java/gov/cdc/dataingestion/model/AuthModel.java
+++ src/main/java/gov/cdc/dataingestion/model/AuthModel.java
@@ -2,33 +2,41 @@ package gov.cdc.dataingestion.model;
 
 public class AuthModel {
 
-    private String clientId;
-    private char[] clientSecret;
+  private String clientId;
+  private char[] clientSecret;
 
-    private String serviceEndpoint;
-    private String requestBody;
+  private String serviceEndpoint;
+  private String requestBody;
 
-    public String getClientId() { return clientId; }
+  public String getClientId() {
+    return clientId;
+  }
 
-            public void setClientId(String username) { this.clientId = username; }
+  public void setClientId(String username) {
+    this.clientId = username;
+  }
 
-    public char[] getClientSecret() { return clientSecret; }
+  public char[] getClientSecret() {
+    return clientSecret;
+  }
 
-    public void setClientSecret(char[] password) { this.clientSecret = password; }
+  public void setClientSecret(char[] password) {
+    this.clientSecret = password;
+  }
 
-    public String getServiceEndpoint() {
-        return serviceEndpoint;
-    }
+  public String getServiceEndpoint() {
+    return serviceEndpoint;
+  }
 
-    public void setServiceEndpoint(String serviceEndpoint) {
-        this.serviceEndpoint = serviceEndpoint;
-    }
+  public void setServiceEndpoint(String serviceEndpoint) {
+    this.serviceEndpoint = serviceEndpoint;
+  }
 
-    public String getRequestBody() {
-        return requestBody;
-    }
+  public String getRequestBody() {
+    return requestBody;
+  }
 
-    public void setRequestBody(String requestBody) {
-        this.requestBody = requestBody;
-    }
+  public void setRequestBody(String requestBody) {
+    this.requestBody = requestBody;
+  }
 }
